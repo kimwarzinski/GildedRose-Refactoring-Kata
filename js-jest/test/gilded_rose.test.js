7821150a3716e8f8283062345c90911208c43ed0
Kim Warzinski <kwarzinski@niche.com>
2020-10-15 13:02:13 -0400

    finished writing test cases and started the refactor by pulling out the base case

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -29,7 +29,7 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(0);
   })
-  it("foo quality does not go negative when degrading twice as fast", function() {
+  it("foo quality does not go below 0 even if given 0", function() {
     const gildedRose = new Shop([new Item("foo", 0, 1)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(0);
@@ -39,10 +39,10 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(59);
   })
-  it("foo does not degrade when negative", function() {
+  it("foo does goes to 0 when negative", function() {
     const gildedRose = new Shop([new Item("foo", 10, -10)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(-10);
+    expect(items[0].quality).toBe(0);
   })
   it("aged brie increases", function() {
     const gildedRose = new Shop([new Item("Aged Brie", 10, 1)]);
@@ -69,9 +69,69 @@ describe("Gilded Rose", function() {
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(54);
   })
-  it("sulfuras", function() {
+  it("sulfuras does not change from 80", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 10, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(80);
+  })
+  it("sulfuras does not change", function() {
     const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 10, 54)]);
     const items = gildedRose.updateQuality();
     expect(items[0].quality).toBe(54);
+  })
+  it("backstage pass increases when there are over 10 days", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 11, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(41);
+  })
+  it("backstage pass increases by 2 when there are 10 days", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(42);
+  })
+  it("backstage pass increases by 2 when there are 9 days", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 9, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(42);
+  })
+  it("backstage pass increases by 2 when there are 6 days", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 6, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(42);
+  })
+  it("backstage pass increases by 3 when there are 5 days", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(43);
+  })
+  it("backstage pass increases by 3 when there are 4 days", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 4, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(43);
+  })
+  it("backstage pass drops to 0 when no days", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  })
+  it("backstage pass drops to 0 when the date has passed", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", -2, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  })
+  it("backstage pass does not go above 50", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  })
+  it("backstage pass does not go above 50", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(50);
+  })
+  it.skip("conjured degrades twice as fast", function() {
+    const gildedRose = new Shop([new Item("Conjured", 5, 49)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(47);
   })
 });
